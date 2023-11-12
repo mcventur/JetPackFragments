71d4d58559a04f143437446eb0a88dea3fa110c5
mcventur <marcelinopenide@gmail.com>
2023-11-12 10:59:32 +0100

    Readme y limpiado MainActivity

--- app/src/main/java/com/mpd/pmdm/jetpackfragments/MainActivity.kt
+++ app/src/main/java/com/mpd/pmdm/jetpackfragments/MainActivity.kt
@@ -4,12 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 const val STAR_SIGN_ID = "STAR_SIGN_ID"
-interface StarSignListener {
-    /**
-     * Se lanzará cuando se seleccione un signo desde el ListFragment
-     */
-    fun onSelected(id: Int)
-}
+
 
 
 class MainActivity : AppCompatActivity() {
